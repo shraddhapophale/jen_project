34c83c22715c884f772e20ccdb3510175e7fb24a
shraddha pophale <shraddhapophale@gmail.com>
2020-01-27 11:46:55 +0530

    hello

--- py1.py
+++ py1.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
 import subprocess
-
-list_files = subprocess.run(["ansible-playbook", "/etc/ansible/abc.yml", "-K"])
+list_files = subprocess.run(["ansible-playbook", "abc.yml", "-K"])
 print("The exit code was: %d" % list_files.returncode)
 if list_files.returncode==0:
 	print("Successful...")
